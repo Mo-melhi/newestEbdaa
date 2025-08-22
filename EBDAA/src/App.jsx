5126c010fa0120008598500d13ae37683cc85cd7
Mo-melhi <m8o9h7.gg5@gmail.com>
2025-08-22 20:25:04 +0300

    meow

---
+++ EBDAA/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useRef } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import MainPage from "./components/MainPage";
+import Marketing from "./components/Marketing";
+import Services from "./components/Services";
+import Printing from "./components/Printing";
+import Footer from "./components/Footer";
+import Features from "./components/Features";
+import WhoWeAre from "./components/WhoWeAre";
+import Carousel from "./components/Carousel";
+import MarketingServicesGrid from './components/MarketingServicesGrid';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Carousel />
+      <Services />
+      <WhoWeAre />
+      <MarketingServicesGrid />
+      <Features />
+      <Footer />
+      
+    </div>
+  );
+}
